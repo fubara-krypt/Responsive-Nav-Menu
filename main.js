ec9a79686c58ac489e663d61dfb127afc3dc3b3a
Fubara <theophilus.dickson13@gmail.com>
2024-07-27 22:03:13 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function openMenu() {
+  document.getElementById("menu-container").style.right = "0%";
+  document.getElementById("menu").style.opacity = "1";
+}
+function closeMenu() {
+  document.getElementById("menu-container").style.right = "-100%";
+  document.getElementById("menu").style.opacity = "0";
+}
